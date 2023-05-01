a3675a42cb9ce046384ac185afe369382f12c028
Zee <81491956+Zatom1@users.noreply.github.com>
2023-04-30 22:28:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,27 +5,16 @@ Created on Tue Apr 12 15:40:11 2022
 @author: zidda
 """
 
-import os
 from notion_client import Client
-from pprint import pprint
-import sys
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
+import calendar
+import datetime as dt
 import requests, json
+import time
 
-def window():
-   app = QApplication(sys.argv)
-   w = QWidget()
-   b = QLabel(w)
-   b.setText("List of Tasks")
-   w.setGeometry(100,100,500,450)
-   b.move(50,20)
-   w.setWindowTitle("PyQt5")
-   w.show()
-   sys.exit(app.exec_())
-   
 
+#date = datetime.datetime.utcnow()
+#utc_time = calendar.timegm(date.utctimetuple())
+#print(utc_time)
 
 notion = Client(auth="secret_UZep77jQV9Atnft7KMSUxgpJaYo7mynPBOBjAEhPBhS")
 list_users_response = notion.users.list()
@@ -46,7 +35,9 @@ taskList = notion.databases.query(
 #pprint(schoolWork)
 schoolworkKeys = list(schoolWork.values())
 
-incompleteTasks = []
+incompleteTaskKeys = []
+taskList = []
+taskWeights = []
 
 for i in range(0, len(schoolworkKeys)+100):
     try:
@@ -54,13 +45,15 @@ for i in range(0, len(schoolworkKeys)+100):
         print(schoolworkKeys[1][i].get("properties").get("Status").get("status").get("name"))
         completeStatus = schoolworkKeys[1][i].get("properties").get("Status").get("status").get("name")
         if completeStatus == "Not started" or completeStatus == "In progress":
-            incompleteTasks.append(completeStatus)
+            incompleteTaskKeys.append(i)
+            taskList.append(schoolworkKeys[1][i].get("properties"))
+            taskWeights.append(0)
     except:
         pass
         #print("task not found") https://www.notion.so/ziddane-isahaku/03c81a69ceb040368bc6afeab26a9c2b?v=5405b26441d74a19871f68ac66dc3976
 #print(schoolworkKeys[1])#[len(schoolworkKeys[1])-2].get("properties").get("Length of Task").get("number"))
 #https://www.notion.so/ziddane-isahaku/d0dba3193f2242b294070cfffb8f22cd?pvs=4
-print(incompleteTasks)
+print(incompleteTaskKeys)
 
 
 headers = {
@@ -69,9 +62,9 @@ headers = {
     "Notion-Version": "2022-06-28"
 }
 
-def updatePage(headers, nthTask, percentToday):
-    updateUrl = "https://api.notion.com/v1/pages/d0dba3193f2242b294070cfffb8f22cd"
-
+def updatePage(headers, nthTask, percentToday, newTitle, url):
+    updateUrl = url
+    print(updateUrl)
     updateData = {
         "properties": {
             "Nth task": {
@@ -84,7 +77,7 @@ def updatePage(headers, nthTask, percentToday):
                 "title": [
                     {
                         "text": {
-                            "content": "wacky"
+                            "content": newTitle
                         }
                     }
                 ]
@@ -95,67 +88,116 @@ def updatePage(headers, nthTask, percentToday):
     data = json.dumps(updateData)
 
     response = requests.request("PATCH", updateUrl, headers=headers, data=data)
-    print(taskList.values())
     #print(response.status_code)
     #print(response.text)
+print(taskList[0])
+
+def insertionSort(arr):
+     
+    if (n := len(arr)) <= 1:
+      return
+    for i in range(1, n):
+         
+        key = arr[i]
+ 
+        # Move elements of arr[0..i-1], that are
+        # greater than key, to one position ahead
+        # of their current position
+        j = i-1
+        while j >=0 and key < arr[j] :
+                arr[j+1] = arr[j]
+                j -= 1
+        arr[j+1] = key
+
+
+
+for i in range(0, len(taskWeights)):
+    taskWeightSum = 0
+    if taskList[i].get("Status").get("status").get("name") == "Not started":
+        taskWeightSum += 50
+    else: 
+        taskWeightSum += 10 
+    try:
+        taskWeightSum += taskList[i].get("Importance").get("number") * 10
+        taskWeightSum += taskList[i].get("Length of Task").get("number") * 10
+        taskWeightSum += 100 - taskList[i].get("Completion %").get("number")
+        
+        #print(taskList[i])
+        #print()
+        date_1 = dt.date.fromisoformat((taskList[i].get("Due date").get("date").get("start")))
+        date_2 = dt.date.today()
+        d = date_1 - date_2 # datetime.timedelta(days=32)
+        taskWeightSum += (14 - d.days)*12
+        #print(d.days)
+        #print()
+    except:
+        print()
+    #daysTillDue = 
+    #print(taskWeightSum)
+    taskWeights[i] += taskWeightSum
+
+for i in range(0, len(taskList)):
+    taskList[i]["weight"] = taskWeights[i]
+    #taskList[i]["date"] = taskWeights[i]
+
+orderedList = sorted(taskList, key=lambda d: d['weight'], reverse = True) 
+sortedWeights = sorted(taskWeights, reverse = True)
+#l = (zip(taskWeights, taskList))
+#importanceList = [taskWeightSum for _, taskWeightSum in sorted(zip(taskList, taskWeights))]
+#taskWeights, taskList = zip(*sorted(zip(taskWeights, taskList)))
+#taskWeights, taskList = (list(t) for t in zip(*sorted(zip(taskWeights, taskList))))
+#for i in range(0, len(orderedList)):
+    #print(orderedList[i].get("Name").get("title")[0].get("text")".get("conten"t"))
     
-updatePage(headers, 5, 50)
+urlList = [
+    "https://api.notion.com/v1/pages/d0dba3193f2242b294070cfffb8f22cd", 
+    "https://api.notion.com/v1/pages/3df2f04fc7ca4c6aa9124aebde0849a2", 
+    "https://api.notion.com/v1/pages/fd70e1484465465897a2a06f913fa62c", 
+    "https://api.notion.com/v1/pages/36d21cce92ff44bb981fd79e509c96a6", 
+    "https://api.notion.com/v1/pages/9f7c1be51ba5416c87126b8c6f365cdf", 
+    "https://api.notion.com/v1/pages/df9b01ebedd44118b29b54973cfa68f5", 
+    "https://api.notion.com/v1/pages/94a14c6750c341bdb0b51cbd74caed1d", 
+    "https://api.notion.com/v1/pages/2a6d13ed4951452d8d73b5595ac798ce", 
+    "https://api.notion.com/v1/pages/d82cfe0b02964d0ca5ff003adea806eb", 
+    "https://api.notion.com/v1/pages/9f77b6a89af64950a1b06224e28a195d"
+    ]
 
+#https://www.notion.so/ziddane-isahaku/task-1-d0dba3193f2242b294070cfffb8f22cd?pvs=4
+#https://www.notion.so/ziddane-isahaku/task-2-3df2f04fc7ca4c6aa9124aebde0849a2?pvs=4
+
+#url0 = urlList[0]
+#url1 = urlList[1]https://www.notion.so/ziddane-isahaku/task-3-fd70e1484465465897a2a06f913fa62c?pvs=4
+#url2 = urlList[2]https://www.notion.so/ziddane-isahaku/task-3-fd70e1484465465897a2a06f913fa62c?pvs=4
+#url3 = urlList[3]
+#url4 = urlList[4]
+#url5 = urlList[5]
+##url6 = urlList[6]
+#url7 = urlList[7]
+#url8 = urlList[8]
+#url9 = urlList[9]
 """
-{'object': 'page', 
- 'id': 'fb4fb0a6-af39-4850-9b63-25ee9fbaff20', 
- created_time': '2023-04-27T19:42:00.000Z', 
- 'last_edited_time': '2023-04-27T19:42:00.000Z', 
- 'created_by': {'object': 'user', 'id': '5be80a0f-38b0-4c72-9e42-c7f134e3afec'}, 
- 'last_edited_by': {'object': 'user', 'id': '5be80a0f-38b0-4c72-9e42-c7f134e3afec'}, 
- 'cover': None, 
- 'icon': None, 
- 'parent': {'type': 'database_id', 'database_id': 'efb13db6-cb8d-4d33-8aa2-3f9eca04d38b'}, 
- 'archived': False, 
- 'properties': {
-     'Course': {
-         'id': 'FROV', 
-         'type': 'relation', 
-         'relation': [{'id': 'bbf103ea-d88b-447b-ad79-dbb08b462c42'}], 
-         'has_more': False
-         }, 
-     'Status': {
-         'id': 'HReV', 
-         'type': 'status', 
-         'status': {
-             'id': '38170805-12ad-4137-a2a2-1bbe445d552f', 
-             'name': 'Done', 
-             'color': 'green'}}, 
-     'Type': {
-         'id': 'K%5ETT', 
-         'type': 'select', 
-         'select': None}, 
-     'Due date': {
-         'id': 'w%5B%3EF', 
-         'type': 'date', 
-         'date': None}, 
-     'Name': {
-         'id': 'title', 
-         'type': 'title', 
-         'title': [{
-             'type': 'text', 
-             'text': {'content': '10.1 Intro to Limits HW', 'link': None}, 
-             'annotations': {
-                 'bold': False, 
-                 'italic': False, 
-                 'strikethrough': False, 
-                 'underline': False, 
-                 'code': False, 
-                 'color': 'default'}, 
-             'plain_text': '10.1 Intro to Limits HW', 'href': None}]}}, 
- 'url': 'https://www.notion.so/10-1-Intro-to-Limits-HW-fb4fb0a6af3948509b6325ee9fbaff20'}
-
+updatePage(headers, 5, 50, "wacky", "https://api.notion.com/v1/pages/d0dba3193f2242b294070cfffb8f22cd")
+updatePage(headers, 5, 50, "wacky", "https://api.notion.com/v1/pages/3df2f04fc7ca4c6aa9124aebde0849a2")
+updatePage(headers, 5, 50, "adsfg", "https://api.notion.com/v1/pages/fd70e1484465465897a2a06f913fa62c")
+updatePage(headers, 5, 50, "wacky", "https://api.notion.com/v1/pages/36d21cce92ff44bb981fd79e509c96a6")
+updatePage(headers, 5, 50, "wacky", "https://api.notion.com/v1/pages/9f7c1be51ba5416c87126b8c6f365cdf")
+updatePage(headers, 5, 50, "wacky", "https://api.notion.com/v1/pages/df9b01ebedd44118b29b54973cfa68f5")
 """
+def minLen(num, list1):
+    if num < len(list1):
+        return num
+    else:
+        return len(list1)
 
-#if __name__ == '__main__':
- #  window()
-
-
-
+for i in range(0, minLen(10, orderedList)):
+    #print(urlList[i])
+    #updatePage(headers, 5, 50, "woah", urlList[i])
+    
+    #next step, make dictionary of task names and completion values, then go through and re-match completion values with names
+    
+    newName = orderedList[i].get("Name").get("title")[0].get("text").get("content")
+    if taskList[i].get("Status").get("status").get("name") == "Done":
+        newName = "awaiting task entry"
+    updatePage(headers, i, 10, newName, urlList[i])
 
 
